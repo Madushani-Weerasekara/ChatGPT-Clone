394196ca161758b8d1f971ba4efe5923d8bd46c2
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-04-25 12:36:02 +0530

    Project setup

---
+++ chatgpt_clone_app.py
@@ -0,0 +1,7 @@
+import os
+import streamlit as st # To create webapplication
+from langchain_openai import OpenAI # To use OpenAI API
+from dotenv import load_dotenv # To load the virtual environment
+load_dotenv()
+
+st.title("ChatGPT-Clone")
