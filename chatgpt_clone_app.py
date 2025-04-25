57341c5e9e7641181d7572cce2b9f7d69edbd6d4
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-04-25 13:25:13 +0530

    Code for chat history

--- chatgpt_clone_app.py
+++ chatgpt_clone_app.py
@@ -5,3 +5,19 @@ from dotenv import load_dotenv # To load the virtual environment
 load_dotenv()
 
 st.title("ChatGPT-Clone")
+
+#set openai api key from stremlit secrets
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+# set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo" #We've set a default
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
